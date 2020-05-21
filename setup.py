17efaa4bb3c0a41fae659c96d8594b0771d56924
Andrew Jones <andyjones.ed@gmail.com>
2020-05-21 11:08:59 +0000

    Gets as far as compilation with cmake

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from Cython.Distutils import build_ext
 
 from distutils.cmd import Command
 from setuptools import find_packages, setup, Extension
-from conda import Runtime
+from pkg import Runtime
 
 try:
     from packaging import get_device_props
